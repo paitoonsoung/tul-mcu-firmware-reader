3ae144a939476d380b38c074a9c95466f7256bf0
Tul <paitoonsoung45@gmail.com>
2026-08-20 23:03:59 +0700

    Add ATmega328P flash read support

--- firmware/tul-mcu-reader/atmega328p_isp.h
+++ firmware/tul-mcu-reader/atmega328p_isp.h
@@ -6,6 +6,7 @@ class Atmega328pIsp {
 public:
     void begin();
     bool readSignature(uint8_t signature[3]);
+    bool readFlashToSd(const char *path = "/ATmega328P_flash.bin");
 
 private:
     uint8_t transfer(uint8_t value);
@@ -13,4 +14,5 @@ private:
     void leaveProgrammingMode();
     void writeBit(uint8_t bit);
     uint8_t readBit();
+    uint8_t readFlashByte(uint16_t wordAddress, bool highByte);
 };
