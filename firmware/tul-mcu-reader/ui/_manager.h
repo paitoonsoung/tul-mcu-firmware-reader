d1e0739376ba59689ed0595e6e287e67f08fee55
Tul <paitoonsoung45@gmail.com>
2026-08-23 21:25:32 +0700

    Fix legacy UI include path and DISPLAY macro conflict

---
+++ firmware/tul-mcu-reader/ui/_manager.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Compatibility wrapper for the legacy include path used by tul-mcu-reader.ino.
+// The canonical UI implementation is in ../ui_manager.h.
+#include "../ui_manager.h"
